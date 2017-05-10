4db47b2384c27a9bb90176e0634f54a2e84e64ae
denilparmar@gmai.com <denilparmar@gmai.com>
2017-05-09 20:17:18 -0400

    added markup for fb login

--- index.php
+++ index.php
@@ -58,7 +58,40 @@ if (@$_GET['q'] && @$_GET['maxResults']) {
 </head>
 <body>
     
+
+
 <form method="GET">
+
+    <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
+    <div class="container">
+        <div class="hero-unit">
+            <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
+        </div>
+        <div class="span4">
+            <ul class="nav nav-list">
+                <li class="nav-header">Image</li>
+                <li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID'];?>/picture?width=150&height=150"></li>
+                <li class="nav-header">Facebook ID</li>
+                <li><?php echo  $_SESSION['FBID']; ?></li>
+                <li class="nav-header">Facebook fullname</li>
+                <li><?php echo $_SESSION['FULLNAME']; ?></li>
+                <li class="nav-header">Facebook Email</li>
+                <li><?php echo $_SESSION['EMAIL']; ?></li>
+                <div><a href="logout.php">Logout</a></div>
+            </ul>
+        </div>
+    </div><?php else: ?>     <!-- Before login -->
+    <div class="container">
+        <h1>Login with Facebook</h1>
+        Not Connected
+        <div>
+            <a href="fbconfig.php">Login with Facebook</a>
+        </div>
+    </div>
+    <?php endif ?>
+
+
+
     <div>
         Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
     </div>
